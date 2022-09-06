8ea1c2ebcbe9037d525cf1511b36aaee6ea33860
Taylor Massey <tamassey92@gmail.com>
2022-09-06 13:03:50 -0400

    commit tree 15869918543f17625d9726627ef24d3c6764d79b

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Write your solution in this file!
+const employee = {
+    name: "",
+    streetAddress: "",
+}
+
+function  updateEmployeeWithKeyAndValue(obj, key, value){
+return {
+...obj, 
+[key]: value,
+};
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value;
+  
+    return obj;
+  }
